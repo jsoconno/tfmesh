c8b18d611616ac40bd7447ebaafb291abc0a5e5a
Justin O'Connor <justinoconnor@justins-air.lan>
2022-01-07 01:44:38 -0500

    push new versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-    required_version = "1.1.3"
+    required_version = "1.0.0"
 
     required_providers {
         aws = {
             source = "hashicorp/aws"
-            version = "3.71.0" # ~>3.0
+            version = "4.1.1" # ~>3.0
         }
     }
     required_providers {
