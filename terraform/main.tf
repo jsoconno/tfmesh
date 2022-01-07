bfa552d79a6bd8a89e42981b6005fd9330a579be
jsoconno <jsoconno@users.noreply.github.com>
2022-01-07 06:36:00 +0000

    Update Terraform module versions.

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,12 +66,12 @@ module "lambda" {
 
 module "consul" {
   source = "hashicorp/consul/aws"
-  version = "1.5.0" # >=0.2.0, <0.6.0
+  version = "0.5.0" # >=0.2.0, <0.6.0
 }
 
 module "conventions" {
   source  = "Jsoconno/conventions/azure"
-  version = "4.0.0"
+  version = "6.0.0"
   # insert the 1 required variable here
 }
 
