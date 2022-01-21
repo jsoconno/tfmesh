b72ff7e4daff88d37f465da74367dc4bb531cb79
Justin O'Connor <justinoconnor@justins-air.lan>
2022-01-20 22:27:29 -0500

    upgrade terraform versions latest with tfmesh

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-    required_version = "1.1.4" # =1.0.0
+    required_version = "1.0.0" # =1.0.0
 
     required_providers {
         aws = {
             source = "hashicorp/aws"
-            version = "3.72.0" # ~>3.0
+            version = "3.73.0" # ~>3.0
         }
     }
     required_providers {
