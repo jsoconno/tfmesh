f5d7e6402377cd5eaf4d1deb3776817356684986
Justin O'Connor <justinoconnor@justins-air.lan>
2022-01-29 21:19:58 -0500

    fix version status calculations

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-    required_version = "1.1.4" # >=1.1.1
+    required_version = "1.1.4" # >1.1.3
 
     required_providers {
         aws = {
             source = "hashicorp/aws"
-            version = "3.74.0" # >=3.0.0, <4.0.0
+            version = "3.74.0" # <=3.0.0, >4.0.0
         }
         azurerm = {
             source = "hashicorp/azurerm"
-            version = "2.94.0" # >1.1.0
+            version = "1.5.0" # =1.1.0
         }
     }
 }
