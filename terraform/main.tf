f9c8ebe7778c383872da304ed545ccc50dbfad2a
Justin O'Connor <justinoconnor@justins-air.lan>
2022-02-06 21:30:07 -0500

    updated semantic versioning and sorting so that regular releases take precedent over pre-releases

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ module "consul" {
 
 module "conventions" {
   source  = "Jsoconno/conventions/azure"
-  version = "0.5.3" # ~>0.4
+  version = "6.0.0" # >=0.5.0
   # insert the 1 required variable here
 }
 
